4c01907a96471c1d0342ec960d4acc7619517200
Thanh Quan <thanhquanky@gmail.com>
2014-03-30 09:41:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-navigator.geolocation.getCurrentPosition(function(pos) {
-  var coords = pos.coords;
-  localStorage.setItem('coords', coords);
-  //var latlongQuery = "?lat=" + coords.latitude + "&long=" + coords.longitude;
-  var lat = coords.latitude;
-  var long = coords.longiute;
-  var testUrl = "http://unispon.com/hackduke/index.php/sepp/walkscore/?lat=" + lat + "&long=" + long;
-  ajax({ url: testUrl, type: 'json' }, function(data) {
-      simply.text({ title: "Walkscore:" + data.walkscore , subtitle: data.message });
-      if (data.workscore > 50) {
-          simply.vibe('short');
-      }
-      else if (data.walkscore > 30) {
-          simply.vibe('long');
-      }
-      else {
-          simply.vibe('long');
-          simply.vibe('long');
-          simply.vibe('long');
-      }
+setInterval(function() {
+  navigator.geolocation.getCurrentPosition(function(pos) {
+    var coords = pos.coords;
+    localStorage.setItem('coords', coords);
+    //var latlongQuery = "?lat=" + coords.latitude + "&long=" + coords.longitude;
+    var lat = coords.latitude;
+    var long = coords.longiute;
+    var testUrl = "http://unispon.com/hackduke/index.php/sepp/walkscore/?lat=" + lat + "&long=" + long;
+    ajax({ url: testUrl, type: 'json' }, function(data) {
+        simply.text({ title: "Walkscore:" + data.walkscore , subtitle: data.message });
+        if (data.workscore > 50) {
+            simply.vibe('short');
+        }
+        else if (data.walkscore > 30) {
+            simply.vibe('long');
+        }
+        else {
+            simply.vibe('long');
+            simply.vibe('long');
+            simply.vibe('long');
+        }
+    });
+  
+    return coords;
   });
-
-  return coords;
-});
+}, 60000);
 
 var count = 0;
 
