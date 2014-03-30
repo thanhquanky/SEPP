af7339210331c78643dc7e2bb7b097ceeb3f2e7b
Thanh Quan <thanhquanky@gmail.com>
2014-03-29 23:25:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var testUrl = "http://unispon.com/hackduke/test.php?test=Thanh&test2=Quan";
+var testUrl = "http://unispon.com/hackduke/test.php?firstName=Thanh&lastName=Quan";
 ajax({ url: testUrl, type: 'json' }, function(data) {
     simply.text({ title: data.teamname, subtitle: data.people });
   });
