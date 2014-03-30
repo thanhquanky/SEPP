d5b452dec9304249f932f1a8e626f35bdd5ab9eb
Thanh Quan <thanhquanky@gmail.com>
2014-03-30 08:39:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,18 @@ navigator.geolocation.getCurrentPosition(function(pos) {
     var long = coords.longiute;
     var testUrl = "http://unispon.com/hackduke/index.php/welcome/walkscore/?lat=" + lat + "&long=" + long;
     ajax({ url: testUrl, type: 'json' }, function(data) {
-        simply.text({ title: "Walkscore" + data.walkscore , subtitle: data.message });
+        simply.text({ title: "Walkscore:" + data.walkscore , subtitle: data.message });
+        if (data.workscore < 30) {
+            simply.vibe('double');
+            simply.vibe('double');
+            simply.vibe('double');
+        }
+        else if (data.walkscore < 50) {
+            simply.vibe('long');
+        }
+        else {
+            simply.vibe('short');
+        }
     });
     
 
