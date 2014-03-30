bab31beae44eb185a9f2001f56bbd285189093db
Thanh Quan <thanhquanky@gmail.com>
2014-03-30 09:10:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 
 */
 
+var api = "http://unispon.com/hackduke/index.php/sepp/"
+
 navigator.geolocation.getCurrentPosition(function(pos) {
     var coords = pos.coords;
+    
+    localStorage.setItem('coords', coords);
     //var latlongQuery = "?lat=" + coords.latitude + "&long=" + coords.longitude;
     var lat = coords.latitude;
     var long = coords.longiute;
-    var testUrl = "http://unispon.com/hackduke/index.php/welcome/walkscore/?lat=" + lat + "&long=" + long;
+    var walkscore_api = api + "walkscore/?lat=" + lat + "&long=" + long;
     ajax({ url: testUrl, type: 'json' }, function(data) {
         simply.text({ title: "Walkscore:" + data.walkscore , subtitle: data.message });
         if (data.workscore > 50) {
@@ -41,8 +45,11 @@ simply.on('singleClick', function(e) {
   if (count >= 5) {
       count = 0;
       simply.subtitle('Calling emergency!!!');
-      var emergency_api = "http://unispon.com/hackduke/index.php/welcome/call";
-      ajax({ url: emergency_api, type: 'json' }, function(data) {
+      var coords = localStorage.get('coords');
+      var emergency_message = "I'm in danger situation. My coords is " + coords.latitute + "," + coords.longitude + ". Please call 911 for me";
+      var emergency_call_api = api + "call";
+      var emergency_text_api = api + "text/?message=" + emergency_message;
+      ajax({ url: emergency_text_api, type: 'json' }, function(data) {
           
       });
   }
