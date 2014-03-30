9c0a30770d40e7f21f1072d1ac69914a570c85a6
Thanh Quan <thanhquanky@gmail.com>
2014-03-30 04:45:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-/*var testUrl = "http://unispon.com/hackduke/test.php?firstName=Thanh&lastName=Quan";
+/*
+var testUrl = "http://unispon.com/hackduke/index.php/walkscore/";
 ajax({ url: testUrl, type: 'json' }, function(data) {
     simply.text({ title: data.teamname, subtitle: data.people });
-  });
+});
 */
 
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
  
-    var latlong = "Lat: " + coords.latitude + ", long: " + coords.longitude ;
+    var latlong = coords.latitude + "," + coords.longitude ;
     simply.text({ title: "Current location", subtitle: latlong });
   
 });
