57c8eb0c56b08148aac6986551237dc8778d4625
Thanh Quan <thanhquanky@gmail.com>
2014-03-30 07:32:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /*
-var testUrl = "http://unispon.com/hackduke/index.php/walkscore/";
-ajax({ url: testUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.teamname, subtitle: data.people });
-});
+
 */
 
 navigator.geolocation.getCurrentPosition(function(pos) {
-  var coords = pos.coords;
- 
-    var latlong = coords.latitude + "," + coords.longitude ;
-    simply.text({ title: "Current location", subtitle: latlong });
+    var coords = pos.coords;
+    var latlongQuery = "?lat="coords.latitude + ",long=" + coords.longitude;
+    var testUrl = "http://unispon.com/hackduke/index.php/walkscore/" + latlongQuery;
+    ajax({ url: testUrl, type: 'json' }, function(data) {
+        simply.text({ title: "Walkscore" , subtitle: data.walkscore });
+    });
+    
   
 });
