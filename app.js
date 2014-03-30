8fcdfbe6e69641015e326956ba2cb18dc6785f83
Thanh Quan <thanhquanky@gmail.com>
2014-03-30 08:30:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ navigator.geolocation.getCurrentPosition(function(pos) {
         simply.text({ title: "Walkscore" + data.walkscore , subtitle: data.message });
     });
     */
-    simple.body(latlongQuery, true);
+    simply.body(latlongQuery, true);
   
 });
