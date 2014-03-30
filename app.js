d73b025ec9550af5eb62ec92e8d3745e88ab8010
Thanh Quan <thanhquanky@gmail.com>
2014-03-29 23:23:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var testUrl = "http://unispon.com/hackduke/test.php";
+var testUrl = "http://unispon.com/hackduke/test.php?test=Thanh&test2=Quan";
 ajax({ url: testUrl, type: 'json' }, function(data) {
     simply.text({ title: data.teamname, subtitle: data.people });
   });
