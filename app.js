0c40025a6bce7e38e49688e067878fcb727526d4
Thanh Quan <thanhquanky@gmail.com>
2014-03-29 22:59:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-simply.title('Hello World!');
+navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+    simply.text({ title: data.name, subtitle: data.main.temp });
+  });
+});
