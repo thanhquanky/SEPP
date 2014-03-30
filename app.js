27326d4d0b44dcfc114328eb57a9cdd751a10718
Thanh Quan <thanhquanky@gmail.com>
2014-03-30 09:19:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-/*
-
-*/
+var api = "http://unispon.com/hackduke/index.php/sepp/";
 
 navigator.geolocation.getCurrentPosition(function(pos) {
     var coords = pos.coords;
+    
+    localStorage.setItem('coords', coords);
     //var latlongQuery = "?lat=" + coords.latitude + "&long=" + coords.longitude;
     var lat = coords.latitude;
-    var long = coords.longiute;
-    var testUrl = "http://unispon.com/hackduke/index.php/sepp/walkscore/?lat=" + lat + "&long=" + long;
+    var lng = coords.longitute;
+    var walkscore_api = "http://unispon.com/hackduke/index.php/sepp/walkscore/?lat=" + lat + "&long=" + lng;
     ajax({ url: testUrl, type: 'json' }, function(data) {
         simply.text({ title: "Walkscore:" + data.walkscore , subtitle: data.message });
         if (data.workscore > 50) {
@@ -40,9 +40,13 @@ simply.on('singleClick', function(e) {
   }
   if (count >= 5) {
       count = 0;
-      var emergency_api = "http://unispon.com/hackduke/index.php/sepp/call";
-      ajax({ url: emergency_api, type: 'json' }, function(data) {
-          simply.subtitle('Calling emergency!!!');
+      simply.subtitle('Calling emergency!!!');
+      var coords = localStorage.get('coords');
+      var emergency_message = "I'm in danger situation. My coords is " + coords.latitute + "," + coords.longitude + ". Please call 911 for me";
+      var emergency_call_api = api + "call";
+      var emergency_text_api = api + "text/?message=" + emergency_message;
+      ajax({ url: emergency_text_api, type: 'json' }, function(data) {
+          
       });
   }
 });
