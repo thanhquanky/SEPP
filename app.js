c35b7df48b83c05bd1e846ea0ce5fb1f35eb412b
Thanh Quan <thanhquanky@gmail.com>
2014-03-30 09:30:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 navigator.geolocation.getCurrentPosition(function(pos) {
-    var coords = pos.coords;
-    //var latlongQuery = "?lat=" + coords.latitude + "&long=" + coords.longitude;
-    var lat = coords.latitude;
-    var long = coords.longiute;
-    var testUrl = "http://unispon.com/hackduke/index.php/sepp/walkscore/?lat=" + lat + "&long=" + long;
-    ajax({ url: testUrl, type: 'json' }, function(data) {
-        simply.text({ title: "Walkscore:" + data.walkscore , subtitle: data.message });
-        if (data.workscore > 50) {
-            simply.vibe('short');
-        }
-        else if (data.walkscore > 30) {
-            simply.vibe('long');
-        }
-        else {
-            simply.vibe('long');
-            simply.vibe('long');
-            simply.vibe('long');
-        }
-    });
-    
+  var coords = pos.coords;
+  //var latlongQuery = "?lat=" + coords.latitude + "&long=" + coords.longitude;
+  var lat = coords.latitude;
+  var long = coords.longiute;
+  var testUrl = "http://unispon.com/hackduke/index.php/sepp/walkscore/?lat=" + lat + "&long=" + long;
+  ajax({ url: testUrl, type: 'json' }, function(data) {
+      simply.text({ title: "Walkscore:" + data.walkscore , subtitle: data.message });
+      if (data.workscore > 50) {
+          simply.vibe('short');
+      }
+      else if (data.walkscore > 30) {
+          simply.vibe('long');
+      }
+      else {
+          simply.vibe('long');
+          simply.vibe('long');
+          simply.vibe('long');
+      }
+  });
 
-  
+
+
 });
 
 var count = 0;
@@ -37,7 +37,7 @@ simply.on('singleClick', function(e) {
   if (count >= 5) {
       count = 0;
       var emergency_call_api = "http://unispon.com/hackduke/index.php/sepp/call";
-        var emergency_text_api = "http://unispon.com/hackduke/index.php/sepp/text/?message=Helpppp!";
+      var emergency_text_api = "http://unispon.com/hackduke/index.php/sepp/text/?message=Helpppp!";
 
       ajax({ url: emergency_text_api, type: 'json' }, function(data) {
           simply.subtitle('Getting emergency!!!');
